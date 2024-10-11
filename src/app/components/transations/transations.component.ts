07c762da6ce70370b449d97df9e5a7c5fb3e7fc8
theRavi01 <ravikantpandey9125@gmail.com>
2024-10-11 09:56:50 +0530

    issue book form

--- src/app/components/transations/transations.component.ts
+++ src/app/components/transations/transations.component.ts
@@ -8,14 +8,22 @@ import { Component } from '@angular/core';
 export class TransationsComponent {
   showBookList: boolean = false; // Initially hide the books list
   showUserList: boolean = false;  // Initially hide the users list
-
+  showIssueBooks: boolean = false;
   showBooks() {
     this.showBookList = true;      // Show books list
     this.showUserList = false;     // Hide users list
+    this.showIssueBooks = false;   // Hide issue book
   }
 
   showUsers() {
     this.showUserList = true;      // Show users list
     this.showBookList = false;     // Hide books list
+    this.showIssueBooks = false;   // Hide issue book
+  }
+
+  showIssueBook() {
+    this.showIssueBooks = true;     // Show issue book form
+    this.showBookList = false;      // Hide books list
+    this.showUserList = false;      // Hide users list
   }
 }
