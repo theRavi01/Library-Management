13e584a6f2929b82e21e7b13bf81d017569307c0
theRavi01 <ravikantpandey9125@gmail.com>
2024-10-11 11:14:08 +0530

    add images

--- src/app/components/transations/transations.component.ts
+++ src/app/components/transations/transations.component.ts
@@ -8,22 +8,33 @@ import { Component } from '@angular/core';
 export class TransationsComponent {
   showBookList: boolean = false; // Initially hide the books list
   showUserList: boolean = false;  // Initially hide the users list
-  showIssueBooks: boolean = false;
+  showIssueBooks: boolean = false; // Initially hide the Issue book form
+  showPayFineForm : boolean = false; // Initially hide the Pay fine form
   showBooks() {
     this.showBookList = true;      // Show books list
     this.showUserList = false;     // Hide users list
     this.showIssueBooks = false;   // Hide issue book
+    this.showPayFineForm = false;      // Hide pay fine 
   }
 
   showUsers() {
     this.showUserList = true;      // Show users list
     this.showBookList = false;     // Hide books list
     this.showIssueBooks = false;   // Hide issue book
+    this.showPayFineForm = false;      // Hide pay fine 
   }
 
   showIssueBook() {
     this.showIssueBooks = true;     // Show issue book form
     this.showBookList = false;      // Hide books list
     this.showUserList = false;      // Hide users list
+    this.showPayFineForm = false;      // Hide pay fine 
+  }
+
+  showPayFine() {
+    this.showIssueBooks = false;     // Hide issue book form
+    this.showBookList = false;      // Hide books list
+    this.showUserList = false;      // Hide users list
+    this.showPayFineForm = true;       //  show pay fine 
   }
 }
